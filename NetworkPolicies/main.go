8fde7b9aa6a8cedb3a2f86d8c3c1c8a43d207478
serkank2 <112072205+serkank2@users.noreply.github.com>
2023-01-22 21:15:02 +0300

    loader

--- NetworkPolicies/main.go
+++ NetworkPolicies/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"os"
 
 	"github.com/gorilla/mux"
 )
@@ -11,17 +10,14 @@ import (
 type handler struct{}
 
 func (h *handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	hostname, err := os.Hostname()
-	if err != nil {
-		fmt.Println(err)
-	}
-	w.Write([]byte("Hello from " + hostname))
+
+	w.Write([]byte("Hello from Testone"))
 }
 
 func main() {
 	mux := mux.NewRouter()
 	mux.Handle("/", &handler{})
 	fmt.Println("Server Started")
-	http.ListenAndServe(":8080", mux)
+	http.ListenAndServe(":9091", mux)
 
 }
